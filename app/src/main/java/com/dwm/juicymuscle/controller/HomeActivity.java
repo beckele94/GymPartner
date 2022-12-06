b1fd1a268d41bf76521f79b17953c4e2154eeb49
AriaAman <ariaaman@outlook.fr>
2022-12-06 15:51:33 +0100

    Affichage et remplissage du recycler view de la page Home avec la liste des programmes des users connectés

--- app/src/main/java/com/dwm/juicymuscle/controller/HomeActivity.java
+++ app/src/main/java/com/dwm/juicymuscle/controller/HomeActivity.java
@@ -1,17 +1,70 @@
 package com.dwm.juicymuscle.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import com.dwm.juicymuscle.R;
+import com.dwm.juicymuscle.adapter.AdapterListExercices;
+import com.dwm.juicymuscle.adapter.AdapterListExoPgrm;
+import com.dwm.juicymuscle.adapter.AdapterListProgrammes;
+import com.dwm.juicymuscle.model.Exercice;
+import com.dwm.juicymuscle.model.ExoPgrm;
+import com.dwm.juicymuscle.model.Programme;
+import com.dwm.juicymuscle.model.PutData;
+import com.dwm.juicymuscle.service.ServiceApi;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+    private ArrayList<Programme> listeProgrammes = new ArrayList<>();
+    private AdapterListProgrammes adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        RecyclerView recyclerView = findViewById(R.id.edittraining_recyclerview_listprogrammes);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new AdapterListProgrammes(listeProgrammes);
+        recyclerView.setAdapter(adapter);
+
+
+        Handler handler = new Handler(); //recuperation des exercices et appel du recycler
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                String[] field = new String[1];
+                String[] data = new String[1];
+                field[0] = "idUser";
+                data[0] = "1"; //TODO : recuper la valeur de l'idUser a partir de l'user connecté (dans la page login)
+                PutData putData = new PutData("https://ulysseguillot.fr/apiLoginJuicyMuscle/getProgrammes.php", "GET", field, data);
+
+                if (putData.startPut()) {
+                    if(putData.onComplete()){
+                        String result = putData.getResult();
+                        ServiceApi jsonToObject = new ServiceApi();
+                        try {
+                            listeProgrammes.addAll(jsonToObject.readJsonProgramme(result));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
