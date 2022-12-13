8d18070269350b001a224ef6debb55dcea693bd6
beckele94 <ulysseguillot@gmail.com>
2022-12-13 21:08:41 +0100

    implementation creation nouveau pgrm (nom ecrit dans une popup window)

--- app/src/main/java/com/dwm/juicymuscle/controller/HomeActivity.java
+++ app/src/main/java/com/dwm/juicymuscle/controller/HomeActivity.java
@@ -9,11 +9,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.PopupMenu;
+import android.widget.EditText;
+import android.widget.PopupWindow;
 
 import com.dwm.juicymuscle.R;
 import com.dwm.juicymuscle.adapter.AdapterListProgrammes;
@@ -34,6 +38,10 @@ public class HomeActivity extends AppCompatActivity {
     private String idUser;
 
     private Button newPgrmBtn;
+    private Button creerPopupBtn;
+    private PopupWindow popupWindow;
+    private EditText nomExoEdit;
+    private String nomExo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,24 +62,55 @@ public class HomeActivity extends AppCompatActivity {
         newPgrmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Handler handler = new Handler(); //recuperation des exercices et appel du recycler
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        String[] field = new String[2];
-                        String[] data = new String[2];
-                        field[0] = "idUser";
-                        data[0] = idUser;
-                        field[1] = "nom";
-                        data[1] = "Nouveau Programme";
-                        PutData putData = new PutData("https://ulysseguillot.fr/apiLoginJuicyMuscle/addProgramme.php", "GET", field, data);
 
-                        if (putData.startPut()) {
-                            if(putData.onComplete()){
-                                Intent HomeActivityIntent = new Intent(HomeActivity.this, HomeActivity.class);
-                                startActivity(HomeActivityIntent);
+                LayoutInflater layoutInflater = (LayoutInflater) HomeActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout._popup_nom_pgrm, null);
+                creerPopupBtn =  customView.findViewById(R.id.popup_button_creer);
+                nomExoEdit = customView.findViewById(R.id.popup_edittext_nompgrm);
+
+                popupWindow = new PopupWindow(customView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
+                popupWindow.showAtLocation(customView, Gravity.CENTER, 0, 0);
+
+                nomExoEdit.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        nomExo = nomExoEdit.getText().toString();
+                    }
+                });
+
+                creerPopupBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Handler handler = new Handler();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                String[] field = new String[2];
+                                String[] data = new String[2];
+                                field[0] = "idUser";
+                                data[0] = idUser;
+                                field[1] = "nom";
+                                data[1] = nomExo;
+                                PutData putData = new PutData("https://ulysseguillot.fr/apiLoginJuicyMuscle/addProgramme.php", "GET", field, data);
+
+                                if (putData.startPut()) {
+                                    if(putData.onComplete()){
+                                        Intent HomeActivityIntent = new Intent(HomeActivity.this, HomeActivity.class);
+                                        startActivity(HomeActivityIntent);
+                                    }
+                                }
                             }
-                        }
+                        });
                     }
                 });
             }
